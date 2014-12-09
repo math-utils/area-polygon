467dd56c5d65e00213c056776ba71b0b6e41e4d0
Jonathan Ong <jonathanrichardong@gmail.com>
2014-12-09 03:12:21 -0800

    refactor package

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var assert = require('assert')
 
-var area = require('./')
+var area = require('..')
 
 describe('Area Polygon', function () {
   it('1x1 square', function () {
@@ -18,17 +18,17 @@ describe('Area Polygon', function () {
   it('3x4 right triangle', function () {
     assert.equal(area([[0,0], [3,0], [3,4]]), 6)
   })
-  
+
   it('anti-clockwise concave polygon, signed param true', function () {
     assert.equal(area([[2,4],[7,0],[18,9],[16,13],[14,10],[11,8],[8,11],[7,15],
       [4,11],[2,4]],true), 108)
   })
-  
+
   it('clockwise concave polygon, signed param true', function () {
     assert.equal(area([[2,4],[4,11],[7,15],[8,11],[11,8],[14,10],[16,13],[18,9],
       [7,0],[2,4]],true), -108)
   })
-  
+
   it('clockwise concave polygon, signed param false', function () {
     assert.equal(area([[2,4],[4,11],[7,15],[8,11],[11,8],[14,10],[16,13],[18,9],
       [7,0],[2,4]],false), 108)
